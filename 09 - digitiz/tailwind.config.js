290d88d1feaf837634b0169e692369b2016bbff3
Mohammad Javad Bahiraei <programmer.bahiraei@gmail.com>
2025-01-04 13:26:10 +0330

    add digitiz project

---
+++ 09 - digitiz/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      // padding: '2rem',
+      padding: {
+        DEFAULT: '.5rem',
+        sm: '1rem',
+        md: '3rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['IRANSans'],
+      },
+    },
+  },
+  plugins: [],
+}
+
